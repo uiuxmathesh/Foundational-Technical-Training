b58bc4241a6b8014c9af9081d910f17e980aa480
uiuxmathesh <matheshpremkumar@gmail.com>
2024-05-17 15:22:33 +0530

    Javascript Day-1

---
+++ Javascript/Day_16.js
@@ -0,0 +1,51 @@
+2 + 3
+5
+var name = "Sai subash"
+undefined
+name
+'Sai subash'
+name + " is super cool 🎉"
+'Sai subash is super cool 🎉'
+2 // 3
+2
+3 ** 2
+9
+typeof(name)
+'string'
+typeof(9)
+'number'
+typeof(true) 
+'boolean'
+typeof(false) // False 
+'boolean'
+var marks = [70, 80, 90, 100]
+undefined
+marks
+(4) [70, 80, 90, 100]
+var student =  {
+    "name" : "Tonika",
+    "batch" : 3
+}
+undefined
+typeof(student)
+'object'
+typeof(marks)
+'object'
+student["name"]
+'Tonika'
+marks[0]
+70
+
+student["name"] // box syntax
+'Tonika'
+student.name
+'Tonika'
+student.name  + ' is in ' + student.batch 
+'Tonika is in 3'
+student.name  + ' is in batch ' + student.batch 
+'Tonika is in batch 3'
+// type casting or coercion
+
+[5, 6, 10] + " nice"
+'5,6,10 nice'
+[5, 6,         10].toString()
